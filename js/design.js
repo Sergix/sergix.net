c911056cb2e0798b1264480b0187ac945caa0f82
Peyton McGinnis <sergix@outlook.com>
2018-06-21 17:39:25 -0400

    AJAX Loading

--- js/design.js
+++ js/design.js
@@ -1,7 +1,11 @@
-var $grid = $('#images').imagesLoaded( function() {
-	$grid.masonry({
-	  itemSelector: '.grid-item',
-	  percentPosition: true,
-	  columnWidth: '.grid-sizer'
-	}); 
-  });
+(function() {
+	
+	var $grid = $('#images').imagesLoaded( function() {
+		$grid.masonry({
+			itemSelector: '.grid-item',
+			percentPosition: true,
+			columnWidth: '.grid-sizer'
+		}); 
+	});
+
+})()
--- js/loader.js
+++ js/loader.js
@@ -1,17 +1,35 @@
-$.ajax({
-    url: '/html/navbar.html',
-    data: null,
-    dataType: "html",
-    success: function(html){
-        $('body').prepend(html);
+(function(){
+    window.onload = function() {
+        document.getElementsByTagName('body')[0].style.display = 'initial';
     }
-});
 
-$.ajax({
-    url: '/html/footer.html',
-    data: null,
-    dataType: "html",
-    success: function(html){
-        $('body').append(html);
+    var xhr_head = new XMLHttpRequest();
+    xhr_head.open('GET', '/html/head.html');
+    xhr_head.send(null);
+
+    xhr_head.onreadystatechange = function () {
+        if (xhr_head.readyState === 4 && xhr_head.status === 200) {
+            document.getElementsByTagName('head')[0].insertAdjacentHTML("afterbegin", xhr_head.responseText);
+        }
     }
-});
+
+    var xhr_navbar = new XMLHttpRequest();
+    xhr_navbar.open('GET', '/html/navbar.html');
+    xhr_navbar.send(null);
+
+    xhr_navbar.onreadystatechange = function () {
+        if (xhr_navbar.readyState === 4 && xhr_navbar.status === 200) {
+            document.getElementsByTagName('body')[0].insertAdjacentHTML("afterbegin", xhr_navbar.responseText);
+        }
+    }
+
+    var xhr_footer = new XMLHttpRequest();
+    xhr_footer.open('GET', '/html/footer.html');
+    xhr_footer.send(null);
+
+    xhr_footer.onreadystatechange = function () {
+        if (xhr_footer.readyState === 4 && xhr_footer.status === 200) {
+            document.getElementsByTagName('body')[0].insertAdjacentHTML("beforeend", xhr_footer.responseText);
+        }
+    }
+})()
