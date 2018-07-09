4041c0b646cea1defe89ffbc5853986b68dda334
Peyton McGinnis <sergix@outlook.com>
2018-07-09 17:09:31 -0400

    Proper Design Loader

--- assets/js/design.js
+++ assets/js/design.js
@@ -4,13 +4,24 @@ $(document).ready(function() {
 	var currentImage;
 	var i = 0;
 	
-	$("img").css("visibility", "hidden");
+	$(".art").css("visibility", "hidden");
+
+	// loader
+	$(".carousel").append('<img id="loader-icon" src="/assets/img/loaders/tail-spin.svg"/>');
+	$(".carousel").append('<figure id="loader-progress">0</figure>');
 	
 	var images = ["break", "canvas5-2", "canvas5-large", "circles", "code-1", "dark", "destiny-unknown", "discover2", "dream-4", "edge-of-tomorrow", "entryflow", "error-nogradient", "fallback", "fear", "frame-of-mind", "insomnia", "jterm", "make", "phone-wallpaper", "reverb2", "revive-this-mind-simple", "root", "sergix", "sergix_destiny1", "sergix-comment", "srgx", "undeniable", "wanderer", "waveform", "wrapper", "zone-theory"];
-	
-	$("img").css("visibility", "visible");
+	var imageFiles = [];
 
 	function loadSlick() {
+		$(".carousel").empty();
+
+		for (i = 0; i < imageFiles.length; i++) {
+			$(".carousel").append(imageFiles[i]);
+		}
+
+		$(".art").css("visibility", "visible");
+
 		$('.carousel').slick({
 			arrows: false,
 			variableWidth: true,
@@ -29,7 +40,8 @@ $(document).ready(function() {
 	}
 	
 	function placeIntoSlick() {
-		$(".carousel").append(currentImage);
+		$("#loader-progress").text( Math.ceil((i / images.length) * 100) );
+		imageFiles.push(currentImage);
 		i++;
 		loadImages();
 	}
@@ -41,6 +53,7 @@ $(document).ready(function() {
 		images[i] = "/assets/img/design/" + images[i] + ".png";
 		currentImage = new Image();
 		currentImage.onload = placeIntoSlick
+		currentImage.className = "art";
 		currentImage.src = images[i];
 	}
 	
