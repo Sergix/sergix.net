f14bd3dc22b49067c5b027761410832777022449
Peyton McGinnis <sergix@outlook.com>
2018-07-09 17:38:04 -0400

    4K Fixes

--- assets/js/project.js
+++ assets/js/project.js
@@ -12,9 +12,9 @@ $(document).ready(function() {
 		$(".row1").wrapAll("<section/>");
 		$(".row2").wrapAll("<section/>");
 		
-		$(".project-info").addClass("col-xs-offset-3");
-		$(".project-info section").addClass("col-xs-12");
-		$(".project-info section section").addClass("col-xs-4");
+		$(".project-info").addClass("col-xs-offset-3 col-lg-offset-1");
+		$(".project-info section").addClass("col-xs-12 col-lg-12");
+		$(".project-info section section").addClass("col-xs-4 col-lg-3");
 		$(".project-info section section").css("margin-right", "5px");
 	}
 });
