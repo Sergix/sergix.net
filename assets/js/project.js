4e47985c277ca38d17f6bf8249f100a919fcb887
Peyton McGinnis <sergix@outlook.com>
2018-07-13 15:57:48 -0400

    Viewport Fixes/Optimizations

--- assets/js/project.js
+++ assets/js/project.js
@@ -1,20 +1,19 @@
-$(document).ready(function() {
+$(document).ready(function () {
 	var options = {
 		arrows: false,
 		autoplay: true,
 		variableWidth: true,
 		rows: 1
 	};
-	
-	if(screen.width < 1024 )  {
+
+	if (window.innerWidth < 768 && window.innerHeight > window.innerWidth) {
 		$(".project-info").slick(options);
 	} else {
 		$(".row1").wrapAll("<section/>");
 		$(".row2").wrapAll("<section/>");
-		
-		$(".project-info").addClass("col-xs-offset-3 col-lg-offset-1");
-		$(".project-info section").addClass("col-xs-12 col-lg-12");
-		$(".project-info section section").addClass("col-xs-4 col-lg-3");
-		$(".project-info section section").css("margin-right", "5px");
+
+		$(".project-info").addClass("col-md-offset-4");
+		$(".project-info > section").addClass("col-sm-12 col-md-12 col-lg-12");
+		$(".project-info > section > section").addClass("col-sm-12 col-md-4 col-lg-4");
 	}
 });
