36667ae98eedb866d054053a533b14d1c119ac30
Peyton McGinnis <sergix@outlook.com>
2018-07-21 23:27:21 -0400

    Small Fix

--- assets/js/project.js
+++ assets/js/project.js
@@ -13,5 +13,8 @@ export function loadProjectPage() {
   } else {
     $('.row1').wrapAll('<section/>')
     $('.row2').wrapAll('<section/>')
+    $('.project-info').addClass('col-xs-offset-4 col-sm-offset-4 col-md-offset-4')
+    $('.project-info > section').addClass('col-sm-12 col-md-12 col-lg-12')
+    $('.project-info > section > section').addClass('col-sm-12 col-md-4 col-lg-4')
   }
 }
