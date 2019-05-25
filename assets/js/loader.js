14ef661a246bb5742fab6443403c4c1c0dc8a913
Peyton McGinnis <sergix@outlook.com>
2019-05-24 21:26:06 -0400

    Icon and General Fixes Update

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -1,103 +1,103 @@
-import "jquery-longshadow";
-import * as Barba from "barba.js";
-import * as design from "./design.js";
-import * as project from "./project.js";
-import * as util from "./util.js";
+import 'jquery-longshadow'
+import * as Barba from 'barba.js'
+import * as design from './design.js'
+import * as project from './project.js'
+import * as util from './util.js'
 
 export const loadPage = (oldTitle, newTitle) => {
   // put the new .page-title in
-  util.changeTitle(document.title.substr(7, document.title.length));
+  util.changeTitle(document.title.substr(7, document.title.length))
 
   // do some class modification for the styling
-  $("#barba-wrapper")
+  $('#barba-wrapper')
     .removeClass(oldTitle)
-    .addClass(newTitle);
+    .addClass(newTitle)
 
-  if (newTitle === "index") {
-    $("#links-open").addClass("index");
-    $("#links-wrapper").addClass("index");
-  } else if ($("#links-open").hasClass("index")) {
-    $("#links-open").removeClass("index");
-    $("#links-wrapper").removeClass("index");
+  if (newTitle === 'index') {
+    $('#links-open').addClass('index')
+    $('#links-wrapper').addClass('index')
+  } else if ($('#links-open').hasClass('index')) {
+    $('#links-open').removeClass('index')
+    $('#links-wrapper').removeClass('index')
   }
 
   // load special content
-  if (newTitle === "design") {
-    design.loadDesign();
-  } else if (newTitle === "project") {
-    project.loadProjectPage();
+  if (newTitle === 'design') {
+    design.loadDesign()
+  } else if (newTitle === 'project') {
+    project.loadProjectPage()
   }
 
-  // some extra styling
-  $("#short-description > h1").longShadow({
-    colorShadow: "#ddd",
+  // long shadow for index page
+  $('#short-description > h1').longShadow({
+    colorShadow: '#ddd',
     sizeShadow: 1000,
-    directionShadow: "bottom-right"
-  });
-};
+    directionShadow: 'bottom-right'
+  })
+}
 
 export const setEventHandlers = () => {
-  util.attachHoverHandler("#links-open");
-  util.attachHoverHandler(".navbar-brand");
+  util.attachHoverHandler('#links-open')
+  util.attachHoverHandler('.navbar-brand')
   //util.attachHoverHandler('')
 
-  util.attachClickHandler("#links-open");
+  util.attachClickHandler('#links-open')
 
-  $(document).on("click touchstart", function(e) {
+  $(document).on('click touchstart', function (e) {
     if (
-      e.target.id !== "links-open" &&
+      e.target.id !== 'links-open' &&
       (e.target.offsetParent === null ||
-        e.target.offsetParent.id !== "links-open")
+        e.target.offsetParent.id !== 'links-open')
     ) {
-      $("#links-open").removeClass("active");
-      $("#links-wrapper").removeClass("active");
+      $('#links-open').removeClass('active')
+      $('#links-wrapper').removeClass('active')
     }
-  });
-};
+  })
+}
 
 export const load = () => {
-  loadPage("", util.getPage());
+  loadPage('', util.getPage())
 
   /* Barba.js handlers */
-  let oldTitle = "";
+  let oldTitle = ''
   const fadeTransition = Barba.BaseTransition.extend({
-    start: function() {
-      oldTitle = util.getPage();
+    start: function () {
+      oldTitle = util.getPage()
 
       Promise.all([this.newContainerLoading, this.fadeOut()]).then(
         this.fadeIn.bind(this)
-      );
+      )
     },
 
-    fadeOut: function() {
+    fadeOut: function () {
       return $(this.oldContainer)
         .animate({ opacity: 0 })
-        .promise();
+        .promise()
     },
 
-    fadeIn: function() {
-      const $el = $(this.newContainer);
+    fadeIn: function () {
+      const $el = $(this.newContainer)
 
-      loadPage(oldTitle, util.getPage());
+      loadPage(oldTitle, util.getPage())
 
-      $(this.oldContainer).hide();
+      $(this.oldContainer).hide()
 
       $el.css({
-        visibility: "visible",
+        visibility: 'visible',
         opacity: 0
-      });
+      })
 
       $el.animate({ opacity: 1 }, 400, () => {
-        this.done();
-      });
+        this.done()
+      })
     }
-  });
+  })
 
-  Barba.Pjax.getTransition = function() {
-    return fadeTransition;
-  };
+  Barba.Pjax.getTransition = function () {
+    return fadeTransition
+  }
 
-  Barba.Pjax.start();
+  Barba.Pjax.start()
 
-  util.bodyLoaderIconEnd();
-};
+  util.bodyLoaderIconEnd()
+}
