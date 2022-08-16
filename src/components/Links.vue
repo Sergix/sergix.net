7d34d5837e8b8fefcf81bbec0c3c681a38b192b4
sergix <me@sergix.dev>
2022-08-16 17:22:41 -0400

    fix: button styling

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -1,30 +1,19 @@
 <template>
   <nav class="flex flex-col items-start">
-    <button @click.stop="$emit('click')">
+    <button class="btn-largelink" @click.stop="$emit('click')">
       <a href="/websites">WEBSITES</a>
     </button>
-    <button @click.stop="$emit('click')">
+    <button class="btn-largelink" @click.stop="$emit('click')">
       <a href="/designs">DESIGNS</a>
     </button>
-    <button @click.stop="$emit('click')">
+    <button class="btn-largelink" @click.stop="$emit('click')">
       <a href="/blog">BLOG</a>
     </button>
   </nav>
 </template>
 
-<style lang="sass" scoped>
-nav > button
-  @apply gradient-primary
-  @apply font-medium leading-none
-  @apply mt-4
-  @apply px-3 pt-2 pb-1
-  @apply text-black text-5xl
-  @apply rounded-lg
-  @apply shadow-md
-
-  &:hover
-    @apply gradient-primary-soft
-
-  &:active
-    @apply gradient-primary-reversed
-</style>
+<script>
+export default {
+  name: 'Links',
+}
+</script>
