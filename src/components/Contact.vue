ee3f7c4df6fb3e6616129ad610a5b36b24eb9c09
sergix <me@sergix.dev>
2022-11-22 14:23:57 -0500

    add: github icon

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -3,5 +3,6 @@
     <h2 class="font-medium mb-2">CONTACT</h2>
     <p>Peyton McGinnis</p>
     <p>hello@sergix.dev</p>
+    <a href="https://github.com/sergix" class="inline-flex flex-row items-center mt-1 bg-zinc-900 hover:bg-zinc-800 active:bg-black hover:px-5 transition-all text-white rounded-sm px-4 py-2 mr-auto" target="blank"><img class="w-5 h-5" src="/image/assets/GitHub-Mark-Light-32px.png" alt="GitHub"/><p class="ml-2">Sergix</p></a>
   </section>
 </template>
