c9590e92cdcd0f5fb86b2e4a3ffff89c88c06b80
sergix <sergix@outlook.com>
2024-07-10 22:17:09 -0400

    add: sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import vue from "@astrojs/vue";
 import tailwind from "@astrojs/tailwind";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), tailwind({
+  site: 'https://sergix.dev',
+  integrations: [vue(), sitemap(), tailwind({
     config: {
       applyBaseStyles: false // disables Tailwind default base stylesheet
     }
